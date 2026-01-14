498c25c73c6c2a238ff78579b4d86660d216fa42
Dan Breczinski <pt2323@gmail.com>
2026-01-14 10:39:27 +0100

    add backend

---
+++ backend/app/public/index.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * This is the central route handler of the application.
+ * It uses FastRoute to map URLs to controller methods.
+ * 
+ * See the documentation for FastRoute for more information: https://github.com/nikic/FastRoute
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use FastRoute\RouteCollector;
+use function FastRoute\simpleDispatcher;
+
+/**
+ * Define the routes for the application.
+ */
+$dispatcher = simpleDispatcher(function (RouteCollector $r) {
+    // Article routes
+    $r->addRoute('GET', '/articles', ['App\Controllers\ArticleController', 'getAll']);
+    $r->addRoute('GET', '/articles/{id}', ['App\Controllers\ArticleController', 'get']);
+    $r->addRoute('POST', '/articles', ['App\Controllers\ArticleController', 'create']);
+    $r->addRoute('PUT', '/articles/{id}', ['App\Controllers\ArticleController', 'update']);
+    $r->addRoute('DELETE', '/articles/{id}', ['App\Controllers\ArticleController', 'delete']);
+});
+
+
+/**
+ * Get the request method and URI from the server variables and invoke the dispatcher.
+ */
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = strtok($_SERVER['REQUEST_URI'], '?');
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+/**
+ * Switch on the dispatcher result and call the appropriate controller method if found.
+ */
+switch ($routeInfo[0]) {
+    // Handle not found routes
+    case FastRoute\Dispatcher::NOT_FOUND:
+        http_response_code(404);
+        echo 'Not Found';
+        break;
+    // Handle routes that were invoked with the wrong HTTP method
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        http_response_code(405);
+        echo 'Method Not Allowed';
+        break;
+    // Handle found routes
+    case FastRoute\Dispatcher::FOUND:
+        $class = $routeInfo[1][0];
+        $method = $routeInfo[1][1];
+        $controller = new $class();
+        $vars = $routeInfo[2];
+        $controller->$method($vars);
+        break;
+}
