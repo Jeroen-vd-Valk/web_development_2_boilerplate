680dc8f408b449c779771ad7ce05697408c7fc69
Dan Breczinski <pt2323@gmail.com>
2026-02-13 16:29:59 +0100

    remove qr code solution

--- backend/app/src/Controllers/QrCodeController.php
+++ backend/app/src/Controllers/QrCodeController.php
@@ -3,15 +3,12 @@
 namespace App\Controllers;
 use App\Framework\Controller;
 
-use chillerlan\QRCode\QRCode;
-
 class QrCodeController extends Controller
 {
 
     public function index()
     {
-        $text = $_GET["text"] ?? "Hello, World!";
-        echo '<img style="width: 400px; height: 400px;" src="'.(new QRCode)->render($text).'" alt="QR Code" />';
+        throw new \Exception("Not implemented yet");
     }
 
 }
